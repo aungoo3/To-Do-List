b55ef1ef7930954d67adfe663a405d6a473a0489
Aung <104800264+aungoo3@users.noreply.github.com>
2023-09-25 16:38:59 -0400

    third commit

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const btnCreate = document.querySelector('.btn-main');
-btnCreate.addEventListener('click', () =>{
-  const input = document.querySelector('.input-main');
-  const list = document.querySelector('ul');
-  
-  list.insertAdjacentHTML(
-    'afterbegin',
-    `<li> ${input.value} </li>`
-  );
-  input.value = ''; 
+const taskList = document.querySelector('.list-container ul');
+const listItems = taskList.children;
+
+function attachRemoveButton(li) {
+  let remove = document.createElement('button');
+  remove.className = 'remove';
+  remove.textContent = 'Delete';
+  li.appendChild(remove);
+}
+
+for (let i = 0; i < listItems.length; i++) {
+  attachRemoveButton(listItems[i]);
+}
+
+taskList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const button = event.target;
+    const li = button.parentNode;
+    li.remove();
+  }
 });
+
+btnCreate.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  const input = document.querySelector('.input-main');
+  let li = document.createElement('li');
+  li.textContent = input.value;
+  attachRemoveButton(li);
+  ul.appendChild(li);
+  input.value = '';
+});
+
 
 //Hide & Show all the lists 
 const btnToggle = document.querySelector('.btn-toggle');
